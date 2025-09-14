8dfb18b1a7b371dad8999e0e2c30411f81c54909
Daiyana <baladaiyana@gmail.com>
2025-09-15 05:28:18 +0530

    second time own commit code

--- src/main/java/SeleniumWork/interviewQuestionPractise/App.java
+++
@@ -1,7 +0,0 @@
-package SeleniumWork.interviewQuestionPractise;
-
-public class App {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
-  }
-}
---
+++ src/main/java/workSpace/codeRewrite.java
@@ -0,0 +1,10 @@
+package workSpace;
+
+public class codeRewrite {
+
+	public static void main(String[] args) {
+		System.out.println("Daiyana"); 
+		
+	}
+
+}
